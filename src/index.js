4205f1d73317e78cd5a1e88a1cd294d50b4d78a3
rafaelitajahy <rafaelitajahy@gmail.com>
2022-10-19 18:01:26 -0300

    add function limit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ const readline = require('readline');
 const ytdl = require('ytdl-core');
 const ffmpeg = require('ffmpeg-static');
 
+function limit(string = '', limit = 0) {
+  return string.substring(0, limit);
+}
+
 const dadosVideo = {
   id: 'RW_4KDIVZ8Q',
   title: '',
-}
+};
 
 const ref = 'https://www.youtube.com/watch?v=RW_4KDIVZ8Q';
+
+ytdl.getInfo(dadosVideo.id).then((info) => {
+  dadosVideo.title = limit(info.videoDetails.title, 100);
+});
+
 const tracker = {
   start: Date.now(),
   audio: { downloaded: 0, total: Infinity },
@@ -16,66 +25,107 @@ const tracker = {
   merged: { frame: 0, speed: '0x', fps: 0 },
 };
 
-ytdl.getInfo(dadosVideo.id).then(info => {
-  dadosVideo.title = info.videoDetails.title;  
-});
-
-
 // Get audio and video streams
-const audio = ytdl(ref, { quality: 'highestaudio' })
-  .on('progress', (_, downloaded, total) => {
+const audio = ytdl(ref, { quality: 'highestaudio' }).on(
+  'progress',
+  (_, downloaded, total) => {
     tracker.audio = { downloaded, total };
-  });
-const video = ytdl(ref, { quality: 'highestvideo' })
-  .on('progress', (_, downloaded, total) => {
+  }
+);
+const video = ytdl(ref, { quality: 'highestvideo' }).on(
+  'progress',
+  (_, downloaded, total) => {
     tracker.video = { downloaded, total };
-  });
+  }
+);
 
 // Prepare the progress bar
 let progressbarHandle = null;
 const progressbarInterval = 1000;
 const showProgress = () => {
   readline.cursorTo(process.stdout, 0);
-  const toMB = i => (i / 1024 / 1024).toFixed(2);
+  const toMB = (i) => (i / 1024 / 1024).toFixed(2);
 
-  process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
-  process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+  process.stdout.write(
+    `Audio  | ${(
+      (tracker.audio.downloaded / tracker.audio.total) *
+      100
+    ).toFixed(2)}% processed `
+  );
+  process.stdout.write(
+    `(${toMB(tracker.audio.downloaded)}MB of ${toMB(
+      tracker.audio.total
+    )}MB).${' '.repeat(10)}\n`
+  );
 
-  process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
-  process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+  process.stdout.write(
+    `Video  | ${(
+      (tracker.video.downloaded / tracker.video.total) *
+      100
+    ).toFixed(2)}% processed `
+  );
+  process.stdout.write(
+    `(${toMB(tracker.video.downloaded)}MB of ${toMB(
+      tracker.video.total
+    )}MB).${' '.repeat(10)}\n`
+  );
 
   process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
-  process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+  process.stdout.write(
+    `(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(
+      10
+    )}\n`
+  );
 
-  process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+  process.stdout.write(
+    `running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(
+      2
+    )} Minutes.`
+  );
   readline.moveCursor(process.stdout, 0, -3);
 };
 
 // Start the ffmpeg child process
-const ffmpegProcess = cp.spawn(ffmpeg, [
-  // Remove ffmpeg's console spamming
-  '-loglevel', '8', '-hide_banner',
-  // Redirect/Enable progress messages
-  '-progress', 'pipe:3',
-  // Set inputs
-  '-i', 'pipe:4',
-  '-i', 'pipe:5',
-  // Map audio & video from streams
-  '-map', '0:a',
-  '-map', '1:v',
-  // Keep encoding
-  '-c:v', 'copy',
-  // Define output file
-  'out.mkv',
-], {
-  windowsHide: true,
-  stdio: [
-    /* Standard: stdin, stdout, stderr */
-    'inherit', 'inherit', 'inherit',
-    /* Custom: pipe:3, pipe:4, pipe:5 */
-    'pipe', 'pipe', 'pipe',
+const ffmpegProcess = cp.spawn(
+  ffmpeg,
+  [
+    // Remove ffmpeg's console spamming
+    '-loglevel',
+    '8',
+    '-hide_banner',
+    // Redirect/Enable progress messages
+    '-progress',
+    'pipe:3',
+    // Set inputs
+    '-i',
+    'pipe:4',
+    '-i',
+    'pipe:5',
+    // Map audio & video from streams
+    '-map',
+    '0:a',
+    '-map',
+    '1:v',
+    // Keep encoding
+    '-c:v',
+    'copy',
+    // Define output file
+    'out.mkv',
   ],
-});
+  {
+    windowsHide: true,
+    stdio: [
+      /* Standard: stdin, stdout, stderr */
+      'inherit',
+      'inherit',
+      'inherit',
+      /* Custom: pipe:3, pipe:4, pipe:5 */
+      'pipe',
+      'pipe',
+      'pipe',
+    ],
+  }
+);
 ffmpegProcess.on('close', () => {
   console.log('done');
   // Cleanup
@@ -85,9 +135,10 @@ ffmpegProcess.on('close', () => {
 
 // Link streams
 // FFmpeg creates the transformer streams and we just have to insert / read data
-ffmpegProcess.stdio[3].on('data', chunk => {
+ffmpegProcess.stdio[3].on('data', (chunk) => {
   // Start the progress bar
-  if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+  if (!progressbarHandle)
+    progressbarHandle = setInterval(showProgress, progressbarInterval);
   // Parse the param=value list returned by ffmpeg
   const lines = chunk.toString().trim().split('\n');
   const args = {};
